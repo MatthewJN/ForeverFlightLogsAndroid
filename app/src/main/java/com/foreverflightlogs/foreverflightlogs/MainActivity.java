392ccc93a1b51413579eb775f19a62af2e945f0b
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2018-10-29 12:55:06 -0600

    test#2

--- app/src/main/java/com/foreverflightlogs/foreverflightlogs/MainActivity.java
+++ app/src/main/java/com/foreverflightlogs/foreverflightlogs/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // For testing - MN
-        //Testing Sheri
+        //Testing Sheri #2
 
     }
 }
