c81730a5028afdbccc025d6933487cd5fc7dc7a9
Steinagel-Addison <absteinagel@gmail.com>
2018-11-01 15:57:47 -0600

    Renamed login activity to UserActivity Renamed enter flight activity to MainActivity Created FlightActivity and its rough UI Created SegmentActivity and its rough UI

--- app/src/main/java/com/foreverflightlogs/foreverflightlogs/UserActivity.java
+++ app/src/main/java/com/foreverflightlogs/foreverflightlogs/UserActivity.java
@@ -3,11 +3,18 @@ package com.foreverflightlogs.foreverflightlogs;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class UserActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_user);
+
+
+
+        //Testing Sheri #2 #3
+        // For testing - MN2
+
+        // Test for Addison
     }
 }
