97d9c5138afacd74bf456d25ace8b956dba20f48
Matthew Newill <matt@gvenue.com>
2018-11-07 14:23:43 +0000

    Syncable Added

--- app/src/main/java/com/foreverflightlogs/foreverflightlogs/FlightPresenter.java
+++ app/src/main/java/com/foreverflightlogs/foreverflightlogs/FlightPresenter.java
@@ -2,7 +2,7 @@ package com.foreverflightlogs.foreverflightlogs;
 
 import java.util.Date;
 
-public class FlightPresenter {
+public class FlightPresenter implements Syncable {
 
     /**
      * The flightID of the current flight.
@@ -33,7 +33,7 @@ public class FlightPresenter {
      * Non-Default Constructor
      * Called when a new flight is starting. Pass in the origin, aircraft, and date.
      *
-     * @param origin The 3 charachter airport code.
+     * @param origin The 3 character airport code.
      * @param aircraft The aircraft tail number.
      * @param startDate The start date (usually the exact time the start button was pressed).
      */
@@ -43,15 +43,15 @@ public class FlightPresenter {
     }
 
     /**
-     * endFlight:
+     * endFlight
      * End the current flight.
      */
     public void endFlight() {
-
+        syncData();
     }
 
     /**
-     * getFlightDuration:
+     * getFlightDuration
      * Provides the duration of the flight. If flight was not ended or has not finished
      * it returns the current duration at the moment the request was made.
      * @return The flight duration in seconds.
@@ -68,4 +68,15 @@ public class FlightPresenter {
 
     }
 
+    /**
+     * syncData
+     * Synchronises the data with the ForeverFlightLog server.
+     * Typically called when a flight ends, but left public in case that fails and
+     * the app needs to try again at a later time.
+     * @return A boolean to indicate if the sync was successful.
+     */
+    @Override
+    public boolean syncData() {
+        return false;
+    }
 }
