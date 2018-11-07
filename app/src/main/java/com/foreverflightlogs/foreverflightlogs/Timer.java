e45bfdcf92a893a9a58476e52f9c5f37b18e02e4
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2018-11-07 08:47:30 -0700

    adding timer test

--- app/src/main/java/com/foreverflightlogs/foreverflightlogs/Timer.java
+++ app/src/main/java/com/foreverflightlogs/foreverflightlogs/Timer.java
@@ -1,4 +1,21 @@
 package com.foreverflightlogs.foreverflightlogs;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class Timer {
+
+    /* Start the timer & return current time
+    *  suggested to use calender here: https://stackoverflow.com/questions/5369682/get-current-time-and-date-on-android
+    *  */
+    public Date start() {
+        //timer should start
+        //get current time and return it
+        Date currentTime = Calendar.getInstance().getTime();
+        /* Not sure format of currentTime
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss.SSS");
+        String test = sdf.format(currentTime);
+        Log.e("TEST", test);*/
+        return currentTime;
+    }
 }
--- app/src/test/java/com/foreverflightlogs/foreverflightlogs/TimerUnitTest.java
+++ app/src/test/java/com/foreverflightlogs/foreverflightlogs/TimerUnitTest.java
@@ -1,4 +1,13 @@
 package com.foreverflightlogs.foreverflightlogs;
 
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertTrue;
+
 public class TimerUnitTest {
+
+    @Test
+    public void startTimer_returnsCurrentTime() { assertTrue(new Timer().start() instanceof Date); }
 }
