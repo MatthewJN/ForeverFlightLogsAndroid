13c6d78bc00c15c91e4b871b30cf05074774dc97
Matthew Newill <matt@gvenue.com>
2018-11-08 08:44:25 +0000

    Adding more SQL Code

--- app/src/main/java/com/foreverflightlogs/foreverflightlogs/FlightPresenter.java
+++ app/src/main/java/com/foreverflightlogs/foreverflightlogs/FlightPresenter.java
@@ -1,12 +1,14 @@
 package com.foreverflightlogs.foreverflightlogs;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import java.util.Date;
 
 public class FlightPresenter implements Syncable {
 
     // Used for testing
     public Date startDate;
-
 
     /**
      * The flightID of the current flight.
@@ -29,7 +31,7 @@ public class FlightPresenter implements Syncable {
      * Pass in a flightID to instantiate the FlightPresenter with the flight desired.
      * @param flightID The flightID assigned by the model.
      */
-    public FlightPresenter(int flightID) {
+    public FlightPresenter(int flightID, Context context) {
 
     }
 
@@ -41,11 +43,24 @@ public class FlightPresenter implements Syncable {
      * @param aircraft The aircraft tail number.
      * @param startDate The start date (usually the exact time the start button was pressed).
      */
-    public FlightPresenter(String origin, String aircraft, Date startDate) {
+    public FlightPresenter(String origin, String aircraft, Date startDate, Context context) {
+
         // Create a new model representing this flight
         // Get an ID and set the private flightID.
+
+        FlightDbHelper mDbHelper = new FlightDbHelper(context);
+
+
         flightID = 0;
         this.startDate = startDate;
+
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(FlightContract.FlightEntry.COLUMN_NAME_STARTDATE, "Date");
+        long newRowID = db.insert(FlightContract.FlightEntry.TABLE_NAME, null, values);
+
+
+
     }
 
     /**
