39f94fd026e0f9f18f11dad7fcf2ac4df8e31f31
Steinagel-Addison <absteinagel@gmail.com>
2018-11-16 15:19:12 -0700

    Switched it so that the app starts with the login screen, then added login() method that will accept the username and password in order to pass them to the UserManager().authWitchAPI method and the Forever Flight Log API, and then displays a message for successful login and starts the next (main) activity

--- app/src/main/java/com/foreverflightlogs/foreverflightlogs/UserActivity.java
+++ app/src/main/java/com/foreverflightlogs/foreverflightlogs/UserActivity.java
@@ -1,7 +1,11 @@
 package com.foreverflightlogs.foreverflightlogs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class UserActivity extends AppCompatActivity {
 
@@ -9,11 +13,29 @@ public class UserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
-
-        // new UserManager().authWithAPI("", "");
     }
 
     public boolean loginUser(String phoneNumber, String password) {
+
+
         return false;
     }
+
+    /** Called when the user taps the login button */
+    public void login(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+        EditText editText2 = (EditText) findViewById(R.id.editText2);
+
+        String username = editText.getText().toString();
+        String password = editText2.getText().toString();
+
+        new UserManager().authWithAPI(username, password, getApplicationContext());
+
+        Intent intent = new Intent(this, MainActivity.class);
+
+        Toast.makeText(this, "Logging in...", Toast.LENGTH_SHORT).show();
+
+        startActivity(intent);
+    }
+
 }
