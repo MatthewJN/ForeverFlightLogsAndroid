853da3eadef293295d1fc726be17ba872fea2884
Matthew Newill <matt@gvenue.com>
2018-11-23 22:55:37 +0000

    Working with Mockito

--- app/src/test/java/com/foreverflightlogs/foreverflightlogs/SegmentPresenterUnitTest.java
+++ app/src/test/java/com/foreverflightlogs/foreverflightlogs/SegmentPresenterUnitTest.java
@@ -4,6 +4,8 @@ import android.widget.Toast;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.util.Date;
 
@@ -16,7 +18,7 @@ public class SegmentPresenterUnitTest {
 
 
 
-        //@Mock
+        @Mock
         private SegmentPresenter sPresenter;
         private Date date;
         private Date startDate;
@@ -25,7 +27,7 @@ public class SegmentPresenterUnitTest {
 
         @Before
         public void setUp() throws Exception {
-            //MockitoAnnotations.initMocks(this);
+            MockitoAnnotations.initMocks(this);
             sPresenter = new SegmentPresenter();
         }
 
