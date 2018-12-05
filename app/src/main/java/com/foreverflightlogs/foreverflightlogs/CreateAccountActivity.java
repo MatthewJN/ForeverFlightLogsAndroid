8a307df434071fd01b7faa67ac456d6fc131935c
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2018-12-05 08:18:52 -0700

    added classes activity and presenter

--- app/src/main/java/com/foreverflightlogs/foreverflightlogs/CreateAccountActivity.java
+++ app/src/main/java/com/foreverflightlogs/foreverflightlogs/CreateAccountActivity.java
@@ -1,4 +1,105 @@
 package com.foreverflightlogs.foreverflightlogs;
 
-public class CreateAccountActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Create Account Activity
+ * User will be able to create an account by providing their
+ * phone number, name, and password.
+ * After successful creation, they will be taken to login screen
+ * where they will login, to create an authorization code to begin
+ * using the site
+ */
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+    }
+
+    /**
+     * Take user to login screen
+     * For users who already have an account
+     */
+    public void onclickLogin(View view){
+        Intent intent = new Intent(this, UserActivity.class);
+        startActivity(intent);
+;    }
+
+    public void createNewAccount(View view){
+        final EditText editPhone = (EditText) findViewById(R.id.phone);
+        final EditText editName = (EditText) findViewById(R.id.name);
+        final EditText editPassword = (EditText) findViewById(R.id.password);
+        final EditText editConfirmPassword = (EditText) findViewById(R.id.confirmPassword);
+        final Button createAccount = (Button) findViewById(R.id.btn_createAccount);
+
+        final String phone = editPhone.getText().toString();
+        final String name = editName.getText().toString();
+        final String password = editPassword.getText().toString();
+        final String confirm = editConfirmPassword.getText().toString();
+
+        //validate input items
+        createAccount.setOnClickListener(new View.OnClickListener() {
+          boolean isValid;
+            @Override
+            public void onClick(View v) {
+                if(phone.length() < 10 ) {
+                    editPhone.setError("Please enter a 10 digit phone number");
+                    isValid = false;
+                    Log.i("PHONE", "phone is: " + phone);
+                    Log.i("PHONELNGTH", "phone.length is: " + phone.length());
+                }
+                if(name.length() == 0) {
+                    editName.setError("Please enter your name");
+                    isValid = false;
+                    Log.i("NAME", "name is: " + name);
+                    Log.i("NAMELNGTH", "name.length is: " + name.length());
+                }
+                if(password.length() == 0){
+                    editPassword.setError("Please enter a password");
+                    isValid = false;
+                    Log.i("PASSWORD", "password is: " + password);
+                    Log.i("PASSWORDLNGTH", "password.length is: " + password.length());
+                }
+                if( confirm.length() == 0 || !password.equals(confirm)){
+                    editConfirmPassword.setError("Passwords do not match. Please enter a password.");
+                    isValid = false;
+                    Log.i("PASSWORD", "password "+ password);
+                    Log.i("CONFIRM", "confirmPassword " + confirm);
+                }
+
+            isValid = true;
+
+                //if passwords don't match display an error
+                if(password.equals(confirm)){
+                    CreateAccountPresenter createAccountPresenter = new CreateAccountPresenter(getApplicationContext());
+                    boolean isAccountCreated = createAccountPresenter.createAccountOnAPI(phone, password, name, getApplicationContext());
+                    if( isAccountCreated){
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        Toast.makeText(getApplicationContext(), "Your account has been created, please login.", Toast.LENGTH_SHORT).show();
+                        startActivity(intent);
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), "Error creating account.", Toast.LENGTH_SHORT).show();
+                        Log.i("ACCTERROR", "Error creating acct: " + createAccountPresenter.getResponse());
+                    }
+                }
+            }
+
+        });
+
+
+
+
+    }
+
 }
--- app/src/main/java/com/foreverflightlogs/foreverflightlogs/CreateAccountPresenter.java
+++ app/src/main/java/com/foreverflightlogs/foreverflightlogs/CreateAccountPresenter.java
@@ -1,4 +1,128 @@
 package com.foreverflightlogs.foreverflightlogs;
 
+import android.content.Context;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+/**
+ * Responsible for  connecting with the API and updating the UI
+ *
+ */
 public class CreateAccountPresenter {
+
+    private Context context;
+    private String response;
+    private boolean isAccountCreated = false;
+
+
+    CreateAccountPresenter(Context context) { this.context = context;}
+
+    public boolean createAccountOnAPI(final String phone, final String password, final String name, final Context context) {
+
+        final Context thisContext = context;
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("https://api.foreverflightlogs.com/v1/accounts");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+                    conn.setRequestProperty("Accept","application/json");
+                    conn.setDoOutput(true);
+                    conn.setDoInput(true);
+
+                    JSONObject jsonParam = new JSONObject();
+                    jsonParam.put("phone", phone); // 5552000000
+                    jsonParam.put("password", password); // test
+                    jsonParam.put("name", name);
+
+                    Log.i("JSONACCT", jsonParam.toString());
+                    DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+                    //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
+                    os.writeBytes(jsonParam.toString());
+
+                    os.flush();
+                    os.close();
+
+                    String reply;
+                    InputStream in = conn.getInputStream();
+                    StringBuffer sb = new StringBuffer();
+                    try {
+                        int chr;
+                        while ((chr = in.read()) != -1) {
+                            sb.append((char) chr);
+                        }
+                        reply = sb.toString();
+                    } finally {
+                        in.close();
+                    }
+                    response = reply;
+                    JSONObject jsonObject = new JSONObject(reply);
+                    String query = jsonObject.getString("query");
+                    String error = jsonObject.getString("error");
+                    Log.i("REPLYFROMACCT", "reply: " + reply);
+                   if (error != null) {
+                       setResponse(reply);
+                      Log.i("ACCTREPLY", "reply is : " + reply);
+
+
+                    }
+                    else {
+                       isAccountCreated = true;
+
+                   }
+
+
+
+                    Log.i("STATUS", String.valueOf(conn.getResponseCode()));
+                    Log.i("MSG" , conn.getResponseMessage());
+                    Log.i("RETURNED", reply);
+
+
+                    conn.disconnect();
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                //return "200";
+            }
+        });
+
+        thread.start();
+    return isAccountCreated;
+    }
+
+    public boolean isAccountCreated() {
+        return isAccountCreated;
+    }
+
+    public void setAccountCreated(boolean accountCreated) {
+        isAccountCreated = accountCreated;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
 }
