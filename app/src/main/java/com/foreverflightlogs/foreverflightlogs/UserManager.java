e7afd873400b83d2c117730fd08fd2b5eb8d7572
Sheri Hansen <10212888+Hansen-M-Sheri@users.noreply.github.com>
2018-12-05 08:20:10 -0700

    Added create account button and logic to user activity

--- app/src/main/java/com/foreverflightlogs/foreverflightlogs/UserManager.java
+++ app/src/main/java/com/foreverflightlogs/foreverflightlogs/UserManager.java
@@ -2,17 +2,13 @@ package com.foreverflightlogs.foreverflightlogs;
 
 import android.content.Context;
 import android.util.Log;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
@@ -28,7 +24,7 @@ public class UserManager {
 
     /**
      * authWithAPI:
-     * Authorises with the API.
+     * Authorizes with the API.
      * @param phoneNumber The phone number (user name).
      * @param password The password for the account.
      * @param context The current context.
