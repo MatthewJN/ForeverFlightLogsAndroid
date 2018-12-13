9abc04519c5c638bedceea83c4fc880ae7788316
Matthew Newill <matt@gvenue.com>
2018-12-13 12:27:16 +0000

    User now gets alerted for bad UN/PW. Also auto moves to Main if already logged in.

--- app/src/main/java/com/foreverflightlogs/foreverflightlogs/UserActivity.java
+++ app/src/main/java/com/foreverflightlogs/foreverflightlogs/UserActivity.java
@@ -3,10 +3,17 @@ package com.foreverflightlogs.foreverflightlogs;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class UserActivity extends AppCompatActivity {
+import java.util.Observable;
+import java.util.Observer;
+
+public class UserActivity extends AppCompatActivity implements Observer {
+
+    UserManager userManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +23,15 @@ public class UserActivity extends AppCompatActivity {
         AirportPresenter airportPresenter = new AirportPresenter();
         airportPresenter.fetchAirports(getApplicationContext());
 
-        UserManager userManager = new UserManager(getApplicationContext());
+        userManager = UserManager.getInstance(getApplicationContext());
+
+//        userManager = new UserManager(getApplicationContext());
         if (userManager.getAuthCode() != "") {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         }
+
+        userManager.addObserver(this);
     }
 
     /** Called when the user taps the login button */
@@ -31,11 +42,40 @@ public class UserActivity extends AppCompatActivity {
         String username = editText.getText().toString();
         String password = editText2.getText().toString();
 
-        UserManager userManager = new UserManager(getApplicationContext());
-        userManager.authWithAPI(username, password, getApplicationContext());
+        boolean isValid = true;
 
+        if (username.length() == 0) {
+            isValid = false;
+        }
+
+        if (password.length() == 0) {
+            isValid = false;
+        }
+
+        if (isValid) {
+            //userManager = new UserManager(getApplicationContext());
+            userManager.authWithAPI(username, password, getApplicationContext());
+        }
+    }
+
+    private void theIntent() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
+    @Override
+    public void update(Observable o, Object arg) {
+
+        if (o != null && o instanceof UserManager) {
+            if (arg == "200" && userManager.getAuthCode() != "") {
+                theIntent();
+            } else {
+                this.runOnUiThread(new Runnable() {
+                    public void run() {
+                        Toast.makeText(UserActivity.this, "Invalid Login Details - Please Try Again", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }
+    }
 }
