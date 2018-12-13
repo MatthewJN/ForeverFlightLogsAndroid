0d4e47e4a00b1b8181b522e8e0e7c7ad0d290495
Matthew Newill <matt@gvenue.com>
2018-12-13 20:14:26 +0000

    Fixed merge issues.

--- app/src/main/java/com/foreverflightlogs/foreverflightlogs/CreateAccountPresenter.java
+++ app/src/main/java/com/foreverflightlogs/foreverflightlogs/CreateAccountPresenter.java
@@ -17,124 +17,122 @@ import java.util.Observable;
 
 /**
  * Responsible for  connecting with the API and updating the UI
- *
  */
 public class CreateAccountPresenter extends Observable {
 
-    private static Context context;
-    private static Object mLock = new Object();
-    private static CreateAccountPresenter CREATE_ACCOUNT_PRESENTER_INSTANCE = null;
-    private String response;
-    private boolean isAccountCreated = false;
+  private static Context context;
+  private static Object mLock = new Object();
+  private static CreateAccountPresenter CREATE_ACCOUNT_PRESENTER_INSTANCE = null;
+  private String response;
+  private boolean isAccountCreated = false;
 
 
-   // CreateAccountPresenter(Context context) { this.context = context;}
-    //turn this into a singleton
-    public static CreateAccountPresenter getInstance(Context aContext) {
-        context = aContext;
-        synchronized (mLock) {
-            if (CREATE_ACCOUNT_PRESENTER_INSTANCE == null) {
-                CREATE_ACCOUNT_PRESENTER_INSTANCE = new CreateAccountPresenter();
+  // CreateAccountPresenter(Context context) { this.context = context;}
+  //turn this into a singleton
+  public static CreateAccountPresenter getInstance(Context aContext) {
+    context = aContext;
+    synchronized (mLock) {
+      if (CREATE_ACCOUNT_PRESENTER_INSTANCE == null) {
+        CREATE_ACCOUNT_PRESENTER_INSTANCE = new CreateAccountPresenter();
+      }
+      return CREATE_ACCOUNT_PRESENTER_INSTANCE;
+    }
+  }
+
+  public void createAccountOnAPI(final String phone, final String password, final String name,
+      final Context context) {
+
+    final Context thisContext = context;
+    Thread thread = new Thread(new Runnable() {
+
+      @Override
+      public void run() {
+        try {
+          URL url = new URL("https://api.foreverflightlogs.com/v1/accounts");
+          HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+          conn.setRequestMethod("POST");
+          conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+          conn.setRequestProperty("Accept", "application/json");
+          conn.setDoOutput(true);
+          conn.setDoInput(true);
+
+          JSONObject jsonParam = new JSONObject();
+          jsonParam.put("phone", phone); // 5552000000
+          jsonParam.put("password", password); // test
+          jsonParam.put("name", name);
+
+          Log.i("JSONACCT", jsonParam.toString());
+          DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+          //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
+          os.writeBytes(jsonParam.toString());
+
+          os.flush();
+          os.close();
+
+          String reply;
+          InputStream in = conn.getInputStream();
+          StringBuffer sb = new StringBuffer();
+          try {
+            int chr;
+            while ((chr = in.read()) != -1) {
+              sb.append((char) chr);
             }
-            return CREATE_ACCOUNT_PRESENTER_INSTANCE;
+            reply = sb.toString();
+          } finally {
+            in.close();
+          }
+          setResponse(reply); //store the reply in case we need to pull in activity
+          JSONObject jsonObject = new JSONObject(reply);
+          if (jsonObject.has("items")) {
+            Log.i("ITEM", "items found in reply");
+          }
+          if (jsonObject.has("error")) {
+            Log.i("ACCTREPLY", "reply is : " + reply);
+            setChanged();
+            notifyObservers("401");
+          } else {
+            setChanged();
+            notifyObservers("200");
+          }
+
+          Log.i("ACCTSTATUS", String.valueOf(conn.getResponseCode()));
+          Log.i("ACCTMSG", conn.getResponseMessage());
+          Log.i("ACCTRETURNED", reply);
+
+          conn.disconnect();
+
+        } catch (MalformedURLException e) {
+          e.printStackTrace();
+        } catch (ProtocolException e) {
+          e.printStackTrace();
+        } catch (IOException e) {
+          setChanged();
+          notifyObservers("401");
+          e.printStackTrace();
+        } catch (JSONException e) {
+          e.printStackTrace();
         }
-    }
+        //return "200";
+      }
+    });
 
-    public void createAccountOnAPI(final String phone, final String password, final String name, final Context context) {
+    thread.start();
 
-        final Context thisContext = context;
-        Thread thread = new Thread(new Runnable() {
+  }
 
-            @Override
-            public void run() {
-                try {
-                    URL url = new URL("https://api.foreverflightlogs.com/v1/accounts");
-                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                    conn.setRequestMethod("POST");
-                    conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
-                    conn.setRequestProperty("Accept","application/json");
-                    conn.setDoOutput(true);
-                    conn.setDoInput(true);
+  public boolean isAccountCreated() {
+    return isAccountCreated;
+  }
 
-                    JSONObject jsonParam = new JSONObject();
-                    jsonParam.put("phone", phone); // 5552000000
-                    jsonParam.put("password", password); // test
-                    jsonParam.put("name", name);
+  public void setAccountCreated(boolean accountCreated) {
+    isAccountCreated = accountCreated;
+  }
 
-                    Log.i("JSONACCT", jsonParam.toString());
-                    DataOutputStream os = new DataOutputStream(conn.getOutputStream());
-                    //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
-                    os.writeBytes(jsonParam.toString());
+  public String getResponse() {
+    return response;
+  }
 
-                    os.flush();
-                    os.close();
-
-                    String reply;
-                    InputStream in = conn.getInputStream();
-                    StringBuffer sb = new StringBuffer();
-                    try {
-                        int chr;
-                        while ((chr = in.read()) != -1) {
-                            sb.append((char) chr);
-                        }
-                        reply = sb.toString();
-                    } finally {
-                        in.close();
-                    }
-                    setResponse(reply); //store the reply in case we need to pull in activity
-                    JSONObject jsonObject = new JSONObject(reply);
-                    if(jsonObject.has("items")){
-                        Log.i("ITEM", "items found in reply");
-                    }
-                    if(jsonObject.has("error")){
-                        Log.i("ACCTREPLY", "reply is : " + reply);
-                        setChanged();
-                        notifyObservers("401");
-                    }
-                    else {
-                        setChanged();
-                        notifyObservers("200");
-                    }
-
-                    Log.i("ACCTSTATUS", String.valueOf(conn.getResponseCode()));
-                    Log.i("ACCTMSG" , conn.getResponseMessage());
-                    Log.i("ACCTRETURNED", reply);
-
-
-                    conn.disconnect();
-
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (ProtocolException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    setChanged();
-                    notifyObservers("401");
-                    e.printStackTrace();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                //return "200";
-            }
-        });
-
-        thread.start();
-
-    }
-
-    public boolean isAccountCreated() {
-        return isAccountCreated;
-    }
-
-    public void setAccountCreated(boolean accountCreated) {
-        isAccountCreated = accountCreated;
-    }
-
-    public String getResponse() {
-        return response;
-    }
-
-    public void setResponse(String response) {
-        this.response = response;
-    }
+  public void setResponse(String response) {
+    this.response = response;
+  }
 }
